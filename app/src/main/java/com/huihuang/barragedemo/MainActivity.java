e26881b3302f02d88cdc5b026da348e82df3198f
jiangziya <jiangziya@gmail.com>
2019-08-16 14:31:54 +0700

    图片视频随意添加弹幕

---
+++ app/src/main/java/com/huihuang/barragedemo/MainActivity.java
@@ -0,0 +1,39 @@
+package com.huihuang.barragedemo;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.huihuang.barragemodule.Barrage;
+import com.huihuang.barragemodule.BarrageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BarrageView barrageView;
+    private List<Barrage> mBarrages = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        barrageView = (BarrageView) findViewById(R.id.barrageView);
+        barrageView.setBarrages(mBarrages);
+        findViewById(R.id.send).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                barrageView.addBarrage(new Barrage("111111111111", R.color.colorPrimary, Color.RED));
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        barrageView.destroy();
+    }
+}
---
+++ barragemodule/src/main/java/com/huihuang/barragemodule/BorderTextView.java
@@ -0,0 +1,33 @@
+package com.huihuang.barragemodule;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+public class BorderTextView extends AppCompatTextView {
+
+    public BorderTextView(Context context, int border_color) {
+        this(context, null, border_color);
+    }
+
+    public BorderTextView(Context context, AttributeSet attrs, int border_color) {
+        super(context, attrs);
+        this.border_color = border_color;
+    }
+
+    private int border_color;
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(border_color);
+        paint.setStrokeWidth(5);
+        canvas.drawLine(0, 0, this.getWidth(), 0, paint);
+        canvas.drawLine(0, 0, 0, this.getHeight(), paint);
+        canvas.drawLine(this.getWidth(), 0, this.getWidth(), this.getHeight(), paint);
+        canvas.drawLine(0, this.getHeight(), this.getWidth(), this.getHeight(), paint);
+        super.onDraw(canvas);
+    }
+}
